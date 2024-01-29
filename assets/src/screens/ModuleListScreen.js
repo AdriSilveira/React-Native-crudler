15a6b425653f03b74daa63339464c0ff95165931
Adri Silveira <73858311+AdriSilveira@users.noreply.github.com>
2024-01-29 16:22:22 +0000

    Adding a flat file

--- assets/src/screens/ModuleListScreen.js
+++ assets/src/screens/ModuleListScreen.js
@@ -1,19 +1,49 @@
-import { StyleSheet, Text } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import Screen from "../layout/Screen";
+import initialModules from "../data/modules.js";
 
 export const ModuleListScreen = () => {
   //Initialisation----------------------------------------
+  const modules = initialModules;
+  const handleSelect = () => alert("Item Selected");
   //State-------------------------------------------------
   //Handlers----------------------------------------------
   //View--------------------------------------------------
 
   return (
     <Screen>
-      <Text>List</Text>
+      <ScrollView style={styles.container}>
+        {modules.map((module) => {
+          return (
+            <Pressable
+              key={module.ModuleCode}
+              style={styles.item}
+              onPress={handleSelect}
+            >
+              <View>
+                <Text style={styles.text}>
+                  {module.ModuleCode}
+                  {module.ModuleName}
+                </Text>
+              </View>
+            </Pressable>
+          );
+        })}
+      </ScrollView>
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  item: {
+    paddingVertical: 15,
+    borderTopWidt: 1,
+    borderColor: "lightgray",
+  },
+  text: {
+    fontSize: 16,
+  },
+});
 
 export default ModuleListScreen;
